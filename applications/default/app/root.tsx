35e94d5aeb484fbf491d00bd9615f3a4541688b8
Matteo Piazza <matteo.piazza@gmail.com>
2025-02-18 16:59:52 +0100

    Custom css variables for custom themes

--- applications/default/app/root.tsx
+++ applications/default/app/root.tsx
@@ -8,6 +8,8 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+// import "@ui/index.css";
+// import "@discharge/ui/src/index.css";
 import "./index.css";
 
 export const links: Route.LinksFunction = () => [
@@ -25,7 +27,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="theme-pink">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
