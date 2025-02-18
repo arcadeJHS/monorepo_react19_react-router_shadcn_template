c085d9ec81ca2998ef9f7458b30638b0f170ec5e
Matteo Piazza <matteo.piazza@gmail.com>
2025-02-18 10:38:32 +0100

    Fixed shadcn layout problems

--- applications/default/vite.config.ts
+++ applications/default/vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./app"),
       "@ui": path.resolve(__dirname, "../../packages/ui/src"),
     },
   },
